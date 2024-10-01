3ba929c282dc052012db5e7957aeb0cd8eb6b380
fzdaze1 <fzdaze@gmail.com>
2024-10-01 18:39:04 +0300

    final_final

--- tests/conftest.py
+++ tests/conftest.py
@@ -20,20 +20,17 @@ TestingSessionLocal = sessionmaker(
 
 @pytest.fixture(scope="session")
 def db():
-    # Создание всех таблиц в тестовой базе данных
     Base.metadata.create_all(bind=engine)
     db = TestingSessionLocal()
     try:
         yield db
     finally:
         db.close()
-        # Удаление всех таблиц после завершения тестов
         Base.metadata.drop_all(bind=engine)
 
 
 @pytest.fixture(scope="module")
 def client(db):
-    # Переопределение зависимости get_db для использования тестовой базы данных
     def override_get_db():
         try:
             yield db
@@ -43,25 +40,21 @@ def client(db):
     app.dependency_overrides[get_db] = override_get_db
     client = TestClient(app)
     yield client
-    # Удаление переопределения зависимости после завершения тестов
     app.dependency_overrides.pop(get_db)
 
 
 @pytest.fixture(scope="session")
 def redis_client():
-    # Инициализация клиента Redis
     redis = Redis(host='localhost', port=6379)
     yield redis
-    redis.close()  # Закрытие соединения Redis после завершения тестов
+    redis.close()
 
 
 @pytest.fixture(scope="session")
 def setup_cache(redis_client):
-    # Инициализация кэша FastAPI перед запуском тестов
     FastAPICache.init(RedisBackend(redis_client), prefix='fastapi-cache')
 
 
 @pytest.fixture(scope="session", autouse=True)
 def init_cache(setup_cache):
-    """Автоматически инициализирует кэш для всех тестов."""
     pass
